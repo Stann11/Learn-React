93b346de7e87992da7259023674d5f3a8f3fcdbd
Stann11 <20it122@charusat.edu.in>
2023-06-05 13:30:43 +0530

    implementation of props in function argument

--- 01-Fundamentals/book/src/index.js
+++ 01-Fundamentals/book/src/index.js
@@ -3,28 +3,51 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 //Props
-const title = "The Book of Charlie";
-const author = "David Von Drehle";
-const img = "./images/book1.jpg";
-const alt = "The Book of Charlie";
+const firstbook = {
+  title: "The Book of Charlie",
+  author: "David Von Drehle",
+  img: "./images/book1.jpg",
+};
+const secondbook = {
+  title: "CROWNED",
+  author: " Kahran Bethencourt",
+  img: "./images/book2.jpg",
+};
+const thirdbook = {
+  title: "Happy Place",
+  author: "Emily Henry",
+  img: "./images/book3.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstbook.author}
+        title={firstbook.title}
+        img={firstbook.img}
+      />
+      <Book
+        author={secondbook.author}
+        title={secondbook.title}
+        img={secondbook.img}
+      />
+      <Book
+        author={thirdbook.author}
+        title={thirdbook.title}
+        img={thirdbook.img}
+      />
       {/* <Author1 /> */}
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={img} alt={alt} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
