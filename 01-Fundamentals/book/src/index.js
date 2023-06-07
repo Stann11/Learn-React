1340fa6c47fb9296cf9e7d3822f337886c873375
Stann11 <20it122@charusat.edu.in>
2023-06-07 16:16:00 +0530

    added button

--- 01-Fundamentals/book/src/index.js
+++ 01-Fundamentals/book/src/index.js
@@ -1,28 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import { books } from "./books.js";
+import Book from "./Book.js";
 //Props
-const books = [
-  {
-    title: "The Book of Charlie",
-    author: "David Von Drehle",
-    img: "./images/book1.jpg",
-    id: 1,
-  },
-  {
-    title: "CROWNED",
-    author: " Kahran Bethencourt",
-    img: "./images/book2.jpg",
-    id: 2,
-  },
-  {
-    title: "Happy Place",
-    author: "Emily Henry",
-    img: "./images/book3.jpg",
-    id: 3,
-  },
-];
 
 const BookList = () => {
   const getBook = (id) => {
@@ -30,22 +11,16 @@ const BookList = () => {
     console.log(book.title);
   };
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
-      })}
-    </section>
-  );
-};
-
-const Book = ({ author, title, img, id, getBook }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      <button onClick={() => getBook(id)}>Buy</button>
-    </article>
+    <>
+      <h1 className="Title">Amazone Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return (
+            <Book {...book} key={book.id} getBook={getBook} number={index} />
+          );
+        })}
+      </section>
+    </>
   );
 };
 
