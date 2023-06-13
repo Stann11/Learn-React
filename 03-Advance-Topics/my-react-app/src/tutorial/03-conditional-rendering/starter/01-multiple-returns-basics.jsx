0b867d134215e0c0a47926bbc7b82f4a9f1c12c7
Stann11 <20it122@charusat.edu.in>
2023-06-13 23:01:43 +0530

    Fetching data from API url

--- 03-Advance-Topics/my-react-app/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ 03-Advance-Topics/my-react-app/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const MultipleReturnsBasics = () => {
+  const [isloading, setIsloading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsloading(false);
+    }, 3000);
+  });
+  if (isloading) {
+    return <h2>Loading...</h2>;
+  }
   return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
--- 03-Advance-Topics/my-react-app/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-Advance-Topics/my-react-app/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,41 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null);
+  const [isloading, setIsloading] = useState(true);
+  const [err, setErr] = useState(false);
+  useEffect(() => {
+    const getuser = async () => {
+      try {
+        let responce = await fetch(url);
+        let temp = await responce.json();
+        setUser(temp);
+      } catch (error) {
+        setErr(true);
+      }
+      setIsloading(false);
+    };
+    getuser();
+  }, []);
+  if (isloading) {
+    return <h1>Loading....</h1>;
+  }
+  if (err) {
+    return <h1>Error 404: Data not found</h1>;
+  }
+  const { login, avatar_url, blog, bio } = user;
+  return (
+    <section>
+      <div>
+        <h2>{login}</h2>
+        <img src={avatar_url}></img>
+        <div>
+          <h4>Works at {blog}</h4>
+          <h5>{bio}</h5>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default MultipleReturnsFetchData;
